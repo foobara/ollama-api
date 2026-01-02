680c4fdaef7c3fe11ef46714f2fbd0149212d707
Miles Georgi <azimux@gmail.com>
2026-01-02 11:28:22 -0800

    Update VCR config

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,7 +1,17 @@
 require "vcr"
 
 VCR.configure do |config|
-  config.filter_sensitive_data("<SCRUBBED_OLLAMA_API_URL>") { ENV.fetch("OLLAMA_API_URL", nil) }
+  config.filter_sensitive_data("<SCRUBBED_OLLAMA_API_URL>") { ENV["OLLAMA_API_URL"] }
+  config.filter_sensitive_data("<SCRUBBED_OLLAMA_API_KEY>") { ENV["OLLAMA_API_KEY"] }
+
+  config.before_record do |interaction|
+    if interaction.request.headers["Cookie"]
+      interaction.request.headers["Cookie"] = ["<SCRUBBED>"]
+    end
+    if interaction.response.headers["Set-Cookie"]
+      interaction.response.headers["Set-Cookie"] = ["<SCRUBBED>"]
+    end
+  end
 
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
